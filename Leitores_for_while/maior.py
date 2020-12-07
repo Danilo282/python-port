954e15c17066228a5a8fef50d65eab10d53bc551
Danilo Silva <nilosilva23@gmail.com>
2020-12-07 11:46:55 -0300

    commit-12

---
+++ Leitores_for_while/maior.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite o seguunto numero: '))
+
+if n1 > n2:
+    print('O numero maior é o: ', n1)
+if n2 > n1:
+    print('O numero maior é o: ', n2)
