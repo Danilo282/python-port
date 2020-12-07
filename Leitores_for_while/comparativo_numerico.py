551cf6ea37c4f849f5e1570deecf3891b3a1a0ba
Danilo Silva <nilosilva23@gmail.com>
2020-12-07 16:08:53 -0300

    commit-14

--- Leitores_for_while/comparativo_numerico.py
+++ Leitores_for_while/comparativo_numerico.py
@@ -3,8 +3,7 @@ n2 = int(input('Digite outro numero: '))
 
 if n1 > n2:
     print('O numero maior é o: ', n1)
-if n2 > n1:
+elif n2 > n1:
     print('O numero maior é o: ', n2)
-    if n1 == n2:
-    else:
-    print('Os numeros sao iguais.')
+elif n1 == n2:
+        print('Os numeros são iguais')
