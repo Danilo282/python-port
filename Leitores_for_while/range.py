9aa4d2ac6d655f5d5cf60a65b9cd324d66e22a67
Danilo Silva <nilosilva23@gmail.com>
2020-12-09 17:28:55 -0300

    commit-23

---
+++ Leitores_for_while/range.py
@@ -0,0 +1,23 @@
+"""
+
+nome = "Danilo Silva"
+lista = (1, 3, 5, 7, 9)
+numero = range(1, 10)
+
+for letra in nome:
+    print(nome)
+
+for numero in lista:
+    print(numero)
+
+for numero in range(1, 10):
+    print(numero)
+
+for a, o in enumerate(nome):
+    print(nome[4])
+
+"""
+nome = 'Danilo Silva'
+
+for letra in enumerate(nome):
+    print(letra)
