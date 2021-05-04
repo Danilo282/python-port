8e59b033f148b3bfdf6b3f2eec61ba6a8cebd0e5
Danilo Pereira da Silva <43241751+Danilo282@users.noreply.github.com>
2021-05-04 02:00:00 -0300

    Update dyna.py

--- Aleatorios/dyna.py
+++ Aleatorios/dyna.py
@@ -3,10 +3,10 @@ import requests_debugger
 
 requests_debugger.set(output_format=requests_debugger.CURL, max_depth=0)
 
-url = 'https://zxu81723.live.dynatrace.com/api/v1/problem/status'
+url = 'https://seusaas.live.dynatrace.com/api/v1/problem/status'
 
 r = requests.get(url,
                  headers={'Authorization':
-                          'Api-Token dt0c01.JK2EK54TFCXRHHULFJCAIVUB.H6C3KKNX47FOWUKKOK6COHQDVFLQODO7LIGKZQBTMYRWJTO6XUQSPFIVU5M56MY4'})
+                          'Api-Token '})
 
 r.json()
