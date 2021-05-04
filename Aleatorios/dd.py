7703f31778c2cdb51ac988f572e8cc0993a2fa9f
Danilo Pereira da Silva <43241751+Danilo282@users.noreply.github.com>
2021-05-04 01:59:06 -0300

    Update dd.py

--- Aleatorios/dd.py
+++ Aleatorios/dd.py
@@ -1,13 +1,13 @@
 from datadog import initialize, api
 
 options = {
-    'api_key': '48d05697469aee246495133dadcbe2a6',
-    'app_key': '323a68f2653b900ab1d1649c255c6dff19d6e30d'
+    'api_key': '',
+    'app_key': ''
 }
 
 initialize(**options)
 
 api.GcpIntegration.delete(
-    project_id="danilo-gcp",
-    client_email="nilosilva23@gmail.com"
+    project_id="",
+    client_email=""
 )
